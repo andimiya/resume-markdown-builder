6e61e00b3947fc41a5a6111461a8b2b0d9ab4e8b
Jon Ah-Nee <jonahnee@gmail.com>
2017-01-03 19:45:07 -1000

    Handlebars added and going to index

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('Server listening on port', port);
+});
