9b96394b13a01d2b4e393be01aef3ee3e7a3eaf4
Jonathan Ah-Nee <jonahnee@gmail.com>
2017-04-13 19:42:22 -1000

    gulp now auto opens and loads index

--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,40 @@
 var gulp = require('gulp');
 var markdown = require('gulp-markdown');
 var minifyCSS = require('gulp-minify-css');
-var nodemon = require('gulp-nodemon');
-var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
 
-gulp.task('start', function() {
-  nodemon({script: 'server.js'})
-    .on('restart', function() {
-      console.log('restarting server');
-      setTimeout(function() {
-        console.log('server restarted');
-      }, 500);
-    });
+gulp.task('watch-files', function() {
+  gulp.watch('./app/*.md', ['markdown']);
+  gulp.watch('./app/scss/*.scss', ['scss']);
 });
 
 gulp.task('markdown', function() {
-  return gulp.src('./app/*.md')
+  gulp.src('./app/*.md')
     .pipe(markdown())
-    .pipe(gulp.dest('public'));
+    .pipe(gulp.dest('public'))
+    .pipe(connect.reload());
 });
 
 gulp.task('scss', function() {
   return gulp.src('./app/scss/*.scss')
     .pipe(sass())
     .pipe(minifyCSS())
-    .pipe(gulp.dest('public'))
+    .pipe(gulp.dest('public'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./app/*.md', ['markdown']);
-  gulp.watch('./app/scss/*.scss', ['scss']);
+gulp.task('open', function () {
+  gulp.src('./public/index.html')
+    .pipe(open({uri: 'http://localhost:8282'}));
 })
 
-gulp.task('default', ['start', 'markdown', 'scss', 'watch'], function() {
-  console.log('tasks started');
+gulp.task('public-server', function (){
+  connect.server({
+    root: './public',
+    port: 8282,
+    livereload: true
+  });
 });
+
+gulp.task('default', ['markdown', 'scss', 'public-server', 'open', 'watch-files']);
